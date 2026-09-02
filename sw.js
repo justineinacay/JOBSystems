f79318bbae471ca08fb52401cbd78808cc47ff86
Justine Inacay <justineluisinacay@gmail.com>
2026-09-02 10:47:06 +0800

    Add interactive Fitness workspace

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 // hadn't changed and CACHE_VERSION was never bumped to force a purge. That's
 // what caused an already-fixed bug (the driveLink Supabase column error) to
 // keep reappearing on-device after the source was already correct.
-const CACHE_VERSION = 'jobsystems-v34';
+const CACHE_VERSION = 'jobsystems-v35';
 const APP_SHELL = [
   './',
   './index.html',
@@ -35,7 +35,8 @@ const APP_SHELL = [
   './mobile-viewport.js?v=20260827-1',
   './agency-command-centers.css?v=20260823-1',
   './faith-command-center.css?v=20260824-2',
-  './life-command-center.css?v=20260826-6',
+  './life-command-center.css?v=20260902-1',
+  './fitness-command-center.css?v=20260902-1',
   './interface-layout-fixes.css?v=20260824-2',
   './ui-coherence-fixes.css?v=20260826-6',
   './today-command-center.css?v=20260826-2',
@@ -45,7 +46,8 @@ const APP_SHELL = [
   './productivity-command-center.css?v=20260901-3',
   './agency-command-centers.js?v=20260823-1',
   './faith-command-center.js?v=20260824-2',
-  './life-command-center.js?v=20260826-2',
+  './life-command-center.js?v=20260902-1',
+  './fitness-command-center.js?v=20260902-1',
   './mobile-command-center.js?v=20260826-3',
   './productivity-command-center.js?v=20260831-4',
 ];
